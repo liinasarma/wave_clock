7008ea141b71e9f4bc38a721888f7b5d034ba961
unknown <lina.sarma@gmail.com>
2019-04-04 18:06:16 +0300

    first files

--- sketch.js
+++ sketch.js
@@ -1,90 +1,70 @@
-let circle;
-let _angnoise, _radiusnoise;
-let _xnoise, _ynoise;
-let _angle = -3.14/2;
+let clock;
+let clock2;
+
+let x1;
+let y1;
 let angle1 = 0;
 let angle2 = 0;
-let _radius;
-let _strokeCol = 254;
-let _strokeChange = -1;
+let strokeCol = 254;
+let strokeChange = -1;
 
 function setup(){
-createCanvas(500,300);
-
-// smooth();
-// frameRate(30);
-background(255);
-noFill();
-
-
-// _angnoise = random(10);
-// _radiusnoise = random(10);
-// _xnoise = random(10);
-// _ynoise = random(10);
-
-
-
-}
-function draw() {
-
-  let p = radians(angle1);
-  let d = radians(angle2);
-  circle = new Circles(p,d);
-  circle.display();
-// _radiusnoise += 0.005;
-// _radius = (noise(_radiusnoise) * 550) +1;
-// _angnoise += 0.005;
- // _angle += (noise(_angnoise) * 6) - 3;
-// if (_angle > 360) { _angle -= 360; }
-// if (_angle < 0) { _angle += 360; }
-
-// _xnoise += 0.01;
-// _ynoise += 0.01;
-// let centerX = width/2 + (noise(_xnoise) * 100) - 50;
-// let centerY = height/2 + (noise(_ynoise) * 100) - 50;
-  // let centerX = 200;
-  // let centerY = 200;
- // let rad = radians(_angle);
-// let x1 = centerX + (_radius * cos(rad));
-// let y1 = centerY + (_radius * sin(rad));
-//  let opprad = rad + PI;
-// let x2 = centerX + (_radius * cos(opprad));
-// let y2 = centerY + (_radius * sin(opprad));
-
+createCanvas(800,600);
+background(210, 140, 150);
 
 
 }
 
-class Circles {
+function draw(){
+x1 = width/2;
+y1 = height/2;
 
-  constructor(rad, rad2) {
+clock = new Clocks(x1, y1);
+clock2 = new Clocks(300,200);
+clock.display();
+clock2.display();
 
-this.r = rad;
-this.r2 = rad2;
-  }
+
+}
+
+class Clocks {
+
+constructor(x,y){
+this.x = x;
+this.y = y;
+
+}
 
 display(){
 
-//   let rad = radians(angle1);
-//   let rad2 = radians(angle2);
-  let scalar = 70;
 
-  let x2 = width / 2 + scalar * cos(this.r);
-  let y2 = height / 2 + scalar * sin(this.r2);
-_strokeCol += _strokeChange;
 
-if (_strokeCol > 254) {
-  _strokeChange = -1; }
-if (_strokeCol < 0) {
-  _strokeChange = 1; }
+let scalar = random(0,70);
 
-  stroke(_strokeCol,60);
+let x2 = width/2+scalar*sin(angle1);
+let y2 = height/2+scalar*cos(angle2);
 
+strokeCol+=strokeChange;
+if(strokeCol>254){
+strokeChange = -1;
+}
+if(strokeCol<0){
+strokeChange = 1;
+}
 strokeWeight(1);
-// line(x1, y1, x2, y2);
-  line(width/2, height/2, x2, y2);
-  angle1++;
-  angle2++;
+stroke(strokeCol,50, strokeCol);
 
-  }
+line(this.x, this.y, x2, y2);
+
+
+angle1++;
+angle2++;
+
+
+
+
+}
+
+
+
 }
