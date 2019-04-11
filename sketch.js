88ede3636277bd2d3dba4bf72bd52061518f453d
unknown <lina.sarma@gmail.com>
2019-04-11 17:13:07 +0300

    adding clocks

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 let clock;
 let clock2;
+let clock3;
+let clock4;
 
 let x1;
 let y1;
+
 let angle1 = 0;
 let angle2 = 0;
-let strokeCol = 254;
+
+let strokeCol1 = 254;
 let strokeChange = -1;
 
 function setup(){
 createCanvas(800,600);
-background(210, 140, 150);
+background(210);
 
 
 }
@@ -19,40 +23,46 @@ function draw(){
 x1 = width/2;
 y1 = height/2;
 
-clock = new Clocks(x1, y1);
-clock2 = new Clocks(300,200);
+clock = new Clocks(100, 100,50, strokeCol1,50,strokeCol1);
+clock2 = new Clocks(300, 100,random(10,60),100,250,strokeCol1);
+clock3 = new Clocks(500, 100,50, strokeCol1,50,strokeCol1);
+clock4 = new Clocks(100, 300,random(10,60),100,250,strokeCol1);
+
 clock.display();
 clock2.display();
-
+clock3.display();
+clock4.display();
 
 }
 
 class Clocks {
 
-constructor(x,y){
+constructor(x,y, scalar, col1, col2, col3){
 this.x = x;
 this.y = y;
+this.scalar = scalar;
+this.col1 = col1;
+this.col2 = col2;
+this.col3 = col3;
 
 }
 
 display(){
 
+let x2 = this.x+this.scalar*sin(angle1*);
+let y2 = this.y+this.scalar*cos(angle1);
 
+this.col1+=strokeChange;
 
-let scalar = random(0,70);
-
-let x2 = width/2+scalar*sin(angle1);
-let y2 = height/2+scalar*cos(angle2);
-
-strokeCol+=strokeChange;
-if(strokeCol>254){
+if(this.col1>254){
 strokeChange = -1;
 }
-if(strokeCol<0){
+if(this.col1<0){
 strokeChange = 1;
 }
+
 strokeWeight(1);
-stroke(strokeCol,50, strokeCol);
+stroke(this.col1,this.col2, this.col3,this.col1);
 
 line(this.x, this.y, x2, y2);
 
